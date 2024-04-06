7bc5160a9dd5b2552e8ee23c8d9d979f0680b422
timur <isaevtimur2016@gmail.com>
2024-04-06 17:59:56 +0300

    gitignore changed

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from app import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('hot/', views.hot, name='hot'),
+    path('question/<int:question_id>', views.question, name='question'),
+    path('ask/', views.ask, name='ask'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('settings/', views.settings, name='settings'),
+]
