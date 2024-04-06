87cb5f7cb5cc8505d9ff3def300b1fceeaa20557
timur <isaevtimur2016@gmail.com>
2024-04-06 17:56:13 +0300

    Homework 1, 2 mistakes fixed

--- Askme_Isaev/urls.py
+++ Askme_Isaev/urls.py
@@ -15,17 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from app import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('hot/', views.hot, name='hot'),
-    path('question/<int:question_id>', views.question, name='question'),
-    path('ask/', views.ask, name='ask'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('settings/', views.settings, name='settings'),
+    path('', include('app.urls')),
     path('admin/', admin.site.urls),
 ]
--- app/views.py
+++ app/views.py
@@ -13,8 +13,16 @@ QUESTIONS = [
 ]
 
 def paginate(objects_list, request, per_page):
-    page_num = request.GET.get('page', 1)
     paginator = Paginator(objects_list, per_page)
+    count = paginator.num_pages
+    try:
+        page_num = int(request.GET.get('page', 1))
+        if page_num <= 0:
+            page_num = 1
+        elif page_num > count:
+            page_num = count
+    except:
+        page_num = 1
     page_obj = paginator.page(page_num)
     return page_obj
 
